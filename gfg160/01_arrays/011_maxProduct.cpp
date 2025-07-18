d3e3936786c4f2e9530b65426bb398a916e43b93
Anish Kumar Barik <barikanish0@gmail.com>
2025-07-19 00:27:15 +0530

    max product of subarray

--- gfg160/01_arrays/011_maxProduct.cpp
+++ gfg160/01_arrays/011_maxProduct.cpp
@@ -4,6 +4,27 @@ using namespace std;
 class Solution {
    public:
     int maxProduct(vector<int> &arr) {
-        // code here
+        int max_prod = INT_MIN;
+        int n = arr.size();
+        int ltr = 1;
+        int rtl = 1;
+
+        for (int i = 0, j = n - 1; i < n && j >= 0; i++, j--) {
+            if (ltr == 0) {
+                ltr = 1;
+            }
+
+            if (rtl == 0) {
+                rtl = 1;
+            }
+
+            ltr *= arr[i];
+            rtl *= arr[j];
+
+            // can use nested max for simplicity
+            max_prod = max({max_prod, ltr, rtl});
+        }
+
+        return max_prod;
     }
 };
