acb3466d642cea8f76632d8b68ed42603ac889d3
Anish Kumar Barik <barikanish0@gmail.com>
2025-08-16 02:19:04 +0530

    sorted and rotated array min

---
+++ gfg160/04_searching/029_findMin.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    int findMin(vector<int>& arr) {
+        int left = 0, right = arr.size() - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] > arr[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return arr[left];
+    }
+};
